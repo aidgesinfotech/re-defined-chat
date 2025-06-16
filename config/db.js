8f6c36857250c19dbea815c730d16017742b7995
Aidges Infotech <153446364+aidgesinfotech@users.noreply.github.com>
2025-06-16 16:29:59 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,17 +3,14 @@ const mysql = require('mysql2');
 
 // Create a connection pool
 const pool = mysql.createPool({
-    host: 'srv1338.hstgr.io',
-    user: 'u895774783_relive',
-    port: 3306,
-    password: 'Redefined@1011',
-    database: 'u895774783_relive',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 510,
     queueLimit: 0 
 });
 
-// Promisify the pool query method for convenience
-const db = pool.promise();
-
-module.exports = db;
+module.exports = pool.promise();
