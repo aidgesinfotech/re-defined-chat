d17c95fa828135e349fcfe44c26dd8cd17a353c8
Aidges Infotech <153446364+aidgesinfotech@users.noreply.github.com>
2025-06-16 14:57:24 +0530

    Update messagesRoutes.js

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const messagesController = require('../controllers/messagesController');
 
 // Route: GET /api/messages/:senderId/:receiverId
+router.get('/activateChatService', messagesController.activateChatService);
 router.get('/:senderId/:receiverId', messagesController.getMessagesBetweenMembers);
 router.post('/', messagesController.sendMessage);
 
