bc15688a61fe8eb630d99cad9f0f6cf59f6a8b5c
Aidges Infotech <153446364+aidgesinfotech@users.noreply.github.com>
2025-07-07 08:37:11 +0530

    Delete models directory

--- models/groupMemberModel.js
+++
@@ -1,9 +0,0 @@
-const db = require('../db');
-
-exports.getGroupMembers = async (groupId) => {
-  const [rows] = await db.query(
-    `SELECT m.* FROM members m
-     JOIN groupMember gm ON m.id = gm.memberId
-     WHERE gm.groupId = ?`, [groupId]);
-  return rows;
-};
--- models/groupModel.js
+++
@@ -1,9 +0,0 @@
-const db = require('../db');
-
-exports.getAllGroupsForMember = async (memberId) => {
-  const [rows] = await db.query(
-    `SELECT g.* FROM groups g
-     JOIN groupMember gm ON g.id = gm.groupId
-     WHERE gm.memberId = ?`, [memberId]);
-  return rows;
-};
